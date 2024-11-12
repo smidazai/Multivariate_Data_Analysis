ea9a8e525f9ca1e8db695cd97b1da543ba1c21e4
smidazai <smida1234@outlook.fr>
2024-11-12 15:48:14 +0100

    Supprimer le répertoire de GitHub mais le garder localement

--- ACP/TP/render_exercises.sh
+++
@@ -1,4 +0,0 @@
-# sheet 1
-quarto render "TP_1.qmd" --output "TP_1_questions.pdf" --profile exercise -P solution:false
-quarto render "TP_1.qmd" --output "TP_1_solutions.pdf" --profile solution
-quarto render "TP_1.qmd" --output "TP_1_master.pdf" --profile solution,exercise --execute-params "code_tools.yaml"
