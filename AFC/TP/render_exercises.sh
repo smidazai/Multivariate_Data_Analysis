25cd9ee8a6d11b43bdcb3c0117e4834e9bf59629
Zaineb <smida1234@outlook.fr>
2025-01-07 14:44:04 +0100

    Supprimer le dossier old de GitHub

--- AFC/TP/render_exercises.sh
+++
@@ -1,4 +0,0 @@
-# sheet 1
-quarto render "TP_2.qmd" --output "TP_2_questions.pdf" --profile exercise -P solution:false
-quarto render "TP_2.qmd" --output "TP_2_solutions.pdf" --profile solution
-quarto render "TP_2.qmd" --output "TP_2_master.pdf" --profile solution,exercise --execute-params "code_tools.yaml"
